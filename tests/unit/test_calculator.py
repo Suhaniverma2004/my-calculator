999bef4ee1ea74bb01897cec23a57bfbdcd82005
Suhaniverma2004 <isuhaniverma@gmail.com>
2025-10-06 17:59:00 +0530

    Changed Multiply and Division Functions with the test cases

--- tests/unit/test_calculator.py
+++ tests/unit/test_calculator.py
@@ -32,3 +32,25 @@ class TestBasicOperations:
 
 # TODO: Students will add TestMultiplyDivide class
 
+class TestMultiplyDivide:
+
+    def test_multiply_positive_numbers(self):
+
+        assert multiply(3, 4) == 12
+        assert multiply(7, 8) == 56
+    def test_multiply_by_zero(self):
+
+        assert multiply(5, 0) == 0
+        assert multiply(0, 10) == 0
+    def test_multiply_negative_numbers(self):
+
+        assert multiply(-2, 3) == -6
+        assert multiply(-4, -5) == 20
+    def test_divide_positive_numbers(self):
+
+        assert divide(10, 2) == 5
+        assert divide(15, 3) == 5
+    def test_divide_negative_numbers(self):
+
+        assert divide(-10, 2) == -5
+        assert divide(-12, -3) == 4
